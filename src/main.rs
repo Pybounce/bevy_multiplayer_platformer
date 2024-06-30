6dfbcf4f6f0912b0f05df829010252ccd17d17dd
Pybounce <pybounce@protonmail.com>
2024-06-30 01:45:27 +0100

    camera added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use bevy::{
+    core_pipeline::bloom::BloomSettings,
+    prelude::*,
+    window::{ close_on_esc, PresentMode },
+    winit::{ UpdateMode, WinitSettings },
+};
+
+
 fn main() {
-    println!("Hello, world!");
+    let winit_settings = WinitSettings {
+        focused_mode: UpdateMode::Continuous,
+        unfocused_mode: UpdateMode::Continuous,
+    };
+    let window_settings = WindowPlugin {
+        primary_window: Some(Window {
+            title: "Legend of the Octo-Parakeet".into(),
+            name: Some("Legend of the Octo-Parakeet".into()),
+            resolution: (1600.0, 900.0).into(),
+            present_mode: PresentMode::Immediate,
+            ..default()
+        }),
+        ..default()
+    };
+
+    
+    App::new()
+        .insert_resource(winit_settings)
+        .add_plugins(DefaultPlugins.set(window_settings))
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, close_on_esc)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands
+        .spawn(Camera2dBundle {
+            camera: Camera {
+                hdr: true,
+                ..default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(BloomSettings::default());
 }
