59fe05fb6ee7ae1ce2372712d2c6d446d374b2ce
Pybounce <pybounce@protonmail.com>
2024-07-06 04:50:33 +0100

    airbourne horizontal movement controller added

--- src/player/horizontal_movement_controller.rs
+++ src/player/horizontal_movement_controller.rs
@@ -9,7 +9,7 @@ pub struct GroundedHorizontalMovementController {
     pub left_key: KeyCode,
     pub right_key: KeyCode,
     pub acceleration: f32,
-    pub friction: f32,
+    pub resistance: f32,
     pub max_speed: f32,
 }
 
@@ -33,7 +33,46 @@ pub fn move_ground_horizontal_controller(
         }
 
         if player_inputting == false {
-            let friction_diff = con.friction * time.delta_seconds();
+            let friction_diff = con.resistance * time.delta_seconds();
+            vel.linvel.x -= vel.linvel.x.signum() * friction_diff.min(vel.linvel.x.abs());
+        }
+
+        vel.linvel.x = vel.linvel.x.abs().min(con.max_speed) * vel.linvel.x.signum();
+    }
+}
+
+
+
+#[derive(Component)]
+pub struct AirbourneHorizontalMovementController {
+    pub left_key: KeyCode,
+    pub right_key: KeyCode,
+    pub acceleration: f32,
+    pub resistance: f32,
+    pub max_speed: f32,
+}
+
+
+pub fn move_airbourne_horizontal_controller(
+    mut query: Query<(&mut Velocity, &AirbourneHorizontalMovementController), Without<Grounded>>,    //todo: need an airbourne state, right now there are seaprate states for jumping
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>
+) {
+    for (mut vel, con) in &mut query {
+
+        let mut player_inputting = false;
+
+        if input.pressed(con.right_key) {
+            vel.linvel.x += con.acceleration * time.delta_seconds();
+            player_inputting = false;
+        }
+        if input.pressed(con.left_key) {
+            vel.linvel.x -= con.acceleration * time.delta_seconds();
+            player_inputting = false;
+        }
+
+        if player_inputting == false {
+            let friction_diff = con.resistance * time.delta_seconds();
             vel.linvel.x -= vel.linvel.x.signum() * friction_diff.min(vel.linvel.x.abs());
         }
 
