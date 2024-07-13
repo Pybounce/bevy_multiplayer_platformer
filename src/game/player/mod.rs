797b277fbef54c9817717a623e5c9490a293811b
Pybounce <pybounce@protonmail.com>
2024-07-13 21:46:56 +0100

    planning player events

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -1,3 +1,19 @@
 
 
 pub mod player_states;
+
+
+// event that kills a player
+    // removes all the controllers from the player (disables movement)
+    // adds a respawn component that has a timer
+        // the respawn component could also house a key input, so they only respawn on key input
+        // timed_respawn component
+        // input_respawn component
+    // spawns death particles etc
+    
+
+// event that spawns a player
+    //adds all the controllers back on the player
+    // checks to see if there is already a player
+        //if so, move them to spawn (reset)
+        //else just spawn a brand new one in
