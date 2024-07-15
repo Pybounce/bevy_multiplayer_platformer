92c97ec68cf01e0a233b4bcdbaa110db19c684bf
Pybounce <pybounce@protonmail.com>
2024-07-15 15:03:13 +0100

    timed despawn component idk

--- src/common/death.rs
+++ src/common/death.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, transform::commands};
 
 #[derive(Component)]
 pub struct DeathMarker;
@@ -12,5 +12,26 @@ pub fn despawn_death_marked(
 ) {
     for e in &query {
         commands.entity(e).despawn();
+    }
+}
+
+
+
+
+#[derive(Component)]
+pub struct TimedDespawn {
+    time_remaining: f32
+}
+
+pub fn update_timed_despawns(
+    mut query: Query<(Entity, &mut TimedDespawn)>,
+    mut commands: Commands,
+    time: Res<Time>
+) {
+    for (e, mut td) in &mut query {
+        td.time_remaining -= time.delta_seconds();
+        if td.time_remaining <= 0.0 {
+            commands.entity(e).try_insert(DeathMarker);
+        }
     }
 }
